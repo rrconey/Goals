919e1ce4f56880df1ec2663efe10f47696f7a89c
Roscoe <rrconey@gmail.com>
2020-03-03 22:00:53 -0800

    adds goal for later button use

--- App.js
+++ App.js
@@ -20,23 +20,19 @@ import {
 } from 'react-native';
 
 import {
+  Card,
+  ListItem,
+  Button,
+  Icon,
+  Avatar,
   Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { Card, ListItem, Button, Icon, Avatar} from 'react-native-elements'
-
-
+} from 'react-native-elements';
 
 
 export default class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={ isLoading: true, dataSource: []}
+    this.state = {isLoading: true, dataSource: []};
   }
 
   componentDidMount(){
@@ -48,166 +44,65 @@ export default class App extends React.Component {
           isLoading: false,
           dataSource: responseJson.movies,
         }, function(){
-
         });
 
       })
-      .catch((error) =>{
+      .catch((error) => {
         console.error(error);
       });
   }
 
-
-
-  render(){
-
-    if(this.state.isLoading){
-      return(
+  render() {
+    if (this.state.isLoading) {
+      return (
         <View style={{flex: 1, padding: 20}}>
           <ActivityIndicator/>
         </View>
-      )
+      );
     }
 
-    return(
+    return (
       <View>
-        <SafeAreaView>
-     
-<Card containerStyle={{padding: 0}} >
-  {
-    this.state.dataSource.map((u, i) => {
-      return (
-        <ListItem
-          key={i}
-          // roundAvatar
-          title={u.title}
-          leftAvatar={{ source: { uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg' } }}
-          subtitle="description"
-          // avatar={{uri:u.avatar}}
+        <Header
+          // leftComponent={{ icon: 'rowing', color: '#fff' }}
+          centerComponent={{text: 'Goals', style: styles.header}}
+          // rightComponent={{ icon: 'home', color: '#fff' }}
+          containerStyle={{
+            // backgroundColor: '#3D6DCC',
+           justifyContent: 'space-around',
+          }}
+
         />
-      );
-    })
-  }
-</Card>
-          
+        <View style={{paddingTop: 20}} >
+          <Text style={{textAlign: 'center' }} >
+            Add Goal
+          </Text>
+        </View>
+<SafeAreaView>
+    <Card containerStyle={{padding: 0}} >
+      {
+        this.state.dataSource.map((u, i) => {
+          return (
+            <ListItem
+              key={i}
+              // roundAvatar
+              title={u.title}
+              leftAvatar={{ source: { uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg' } }}
+              subtitle="description"
+              // avatar={{uri:u.avatar}}
+            />
+          );
+        })
+      }
+    </Card>
         </SafeAreaView>
       </View>
     );
   }
 }
 
-
-
-
-
-
-
-
-
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {isLoading: true, dataSource: []};
-//   }
-
-//   componentDidMount(){
-//     return fetch('https://reactnative.dev/movies.json')
-//       .then((response) => response.json())
-//       .then((responseJson) => {
-
-//       this.setState({
-//           isLoading: false,
-//           dataSource: responseJson.movies,
-//         }, function(){
-
-//         });
-
-//       })
-//       .catch((error) =>{
-//         console.error(error);
-//       });
-//   }
-
-//   render() {
-//     return (
-//       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//         <SafeAreaView>
-//           <ScrollView
-//             contentInsetAdjustmentBehavior="automatic"
-//             style={styles.scrollView}>
-//             <Header />
-//             {global.HermesInternal == null ? null : (
-//               <View style={styles.engine}>
-//                 <Text style={styles.footer}>Engine: Hermes</Text>
-//               </View>
-//             )}
-//             <View style={styles.body}>
-//               <View style={styles.sectionContainer}>
-//                 <Text style={styles.sectionTitle}>
-//                   Step One - {this.state.dataSource[0].title}
-//                 </Text>
-//                 <Text style={styles.sectionDescription}>
-//                   Edit <Text style={styles.highlight}>App.js</Text> to change this
-//                   screen and then come back to see your edits.
-//                 </Text>
-//               </View>
-//               <View style={styles.sectionContainer}>
-//                 <Text style={styles.sectionTitle}>See Your Changes</Text>
-//                 <Text style={styles.sectionDescription}>
-//                   <ReloadInstructions />
-//                 </Text>
-//               </View>
-//               <View style={styles.sectionContainer}>
-//                 <Text style={styles.sectionTitle}>Debug</Text>
-//                 <Text style={styles.sectionDescription}>
-//                   <DebugInstructions />
-//                 </Text>
-//               </View>
-              
-//               <LearnMoreLinks />
-//             </View>
-//           </ScrollView>
-//       </SafeAreaView>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
+const styles = StyleSheet.create({
+  header: {
+    color: '#fff'
+  }
+})
